00f96e9a94bc4169070f07778f72ad5006b3bd7c
Dorothy Jung <jungy@berkeley.edu>
2015-07-20 18:48:05 +0200

    test database functions

--- index.js
+++ index.js
@@ -31,16 +31,8 @@ app.get('/realtime', function(request, response) {
 });
 
 app.get('/run_trial', function(request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/run_trial', {results: result.rows} ); }
-    });
-  });
-})
+  response.render('pages/run_trial');
+});
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
@@ -52,7 +44,7 @@ app.get('/db', function (request, response) {
        { response.render('pages/db', {results: result.rows} ); }
     });
   });
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
