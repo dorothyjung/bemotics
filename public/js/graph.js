e567d4f54447b2bd3833102c5dc93c67b8494004
Dorothy Jung <jungy@berkeley.edu>
2015-07-20 13:52:38 +0200

    added static graph of sample eeg data

--- public/js/graph.js
+++ public/js/graph.js
@@ -10,21 +10,12 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
 
   // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Topping');
-  data.addColumn('number', 'Slices');
-  data.addRows([
-    ['Mushrooms', 3],
-    ['Onions', 1],
-    ['Olives', 1],
-    ['Zucchini', 1],
-    ['Pepperoni', 2]
-  ]);
+  var data = google.visualization.arrayToDataTable(
+    [['seconds', 'Engagement', 'Frustration', 'Meditation', 'Excitement'], ['1.000', '0.641123', '0', '0.316708', '0.00186492'], ['2.000', '0.641123', '0', '0.316708', '0.00186492'], ['3.000', '0.641123', '0', '0.316708', '0.00186492'], ['4.000', '0.720896', '0', '0.301749', '0.00577296'], ['5.000', '0.720896', '0', '0.301749', '0.00577296'], ['6.000', '0.720896', '0', '0.301749', '0.00577296'], ['7.000', '0.720896', '0', '0.301749', '0.00577296'], ['8.000', '0.720896', '0', '0.301749', '0.00577296'], ['9.000', '0.747991', '0', '0.296636', '0.0114218'], ['10.000', '0.747991', '0', '0.296636', '0.0114218'], ['11.000', '0.747991', '0', '0.296636', '0.0114218'], ['12.000', '0.747991', '0', '0.296636', '0.0114218'], ['13.000', '0.75829', '0', '0.299419', '0.0180803'], ['14.000', '0.75829', '0', '0.299419', '0.0180803'], ['15.000', '0.767428', '0', '0.302485', '0.0411644'], ['16.000', '0.767428', '0', '0.302485', '0.0411644'], ['17.000', '0.767428', '0', '0.302485', '0.0411644'], ['18.000', '0.768346', '0', '0.305284', '0.0733166']]
+  );
 
   // Set chart options
-  var options = {'title':'How Much Pizza I Ate Last Night',
-                 'width':400,
-                 'height':300};
+  var options = {'title':'Brainwave Readings'};
 
   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
   chart.draw(data, options);
--- public/js/headsetInteraction.js
+++ public/js/headsetInteraction.js
@@ -1,3 +1,7 @@
+function ELSPlugin(){
+  return document.getElementById('plugin0')
+}
+
 function checkPluginExits(){
   var L = navigator.plugins.length;
   for(var i = 0; i < L; i++) {
@@ -39,41 +43,20 @@ $(document).ready(function(e){
   updateEmoEngine();
 });
 
-var lastResponseIndex = -1;
 function updateEmoEngine(){   
 
-    engine.ProcessEvents(500);
+  engine.ProcessEvents(500);
 
-    console.log("Engagement: " + es.AffectivGetEngagementBoredomScore())
-    console.log("Frustration: " + es.AffectivGetFrustrationScore())
-    console.log("Short term Excitement: " + es.AffectivGetExcitementShortTermScore())
-    console.log("Long term Excitement: " + es.AffectivGetExcitementLongTermScore())
+  var EngagementBoredomScore = es.AffectivGetEngagementBoredomScore()
+  var FrustrationScore = es.AffectivGetFrustrationScore()
+  var ExcitementShortTermScore = es.AffectivGetExcitementShortTermScore()
+  var ExcitementLongTermScore = es.AffectivGetExcitementLongTermScore()
 
-    //Engagement: 0
-    if (es.AffectivGetEngagementBoredomScore() > .8 && lastResponseIndex !== 0){
-        console.log("Engagement: " + es.AffectivGetEngagementBoredomScore())
-        lastResponseIndex = 0
-        //plot Engagement
-    }
-    //Frustration: 1
-    if (es.AffectivGetFrustrationScore() > .8 && lastResponseIndex !== 1){
-        console.log("Frustration: " + es.AffectivGetFrustrationScore())
-        lastResponseIndex = 1
-        //plot Frustration
-    }
-    //Short term excitement: 2
-    else if (es.AffectivGetExcitementShortTermScore() > .8 && lastResponseIndex !== 2){
-        console.log("Short term Excitement: " + es.AffectivGetExcitementShortTermScore())
-        lastResponseIndex = 2
-        //plot ShortTerm
-    }
-    //Long term excitement: 3
-    else if (es.AffectivGetExcitementLongTermScore() > .8 && lastResponseIndex !== 3){
-        console.log("Long term Excitement: " + es.AffectivGetExcitementLongTermScore())
-        lastResponseIndex = 3
-        //plot LongTerm
-    }
-    
-    setTimeout("updateEmoEngine()",50);
+  console.log("Engagement: " + EngagementBoredomScore)
+  console.log("Frustration: " + FrustrationScore)
+  console.log("Short term Excitement: " + ExcitementShortTermScore)
+  console.log("Long term Excitement: " + ExcitementLongTermScore)
+  
+  setTimeout("updateEmoEngine()",50);
 }
 
