35d5983e12f906cfe9ed8fb0bc69cb73585f0c0c
Alice Ma <ma.alice95@gmail.com>
2015-07-22 00:11:26 +0200

    alice's template changes and charts

--- index.js
+++ index.js
@@ -30,6 +30,35 @@ app.get('/realtime', function(request, response) {
   response.render('pages/realtime');
 });
 
+app.get('/create_participant', function(request, response) {
+  response.render('pages/create_participant');
+});
+
+app.get('/create_project', function(request, response) {
+  response.render('pages/create_project');
+});
+
+app.get('/participants', function(request, response) {
+  response.render('pages/participants');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
+
+app.get('/start_test', function(request, response) {
+  response.render('pages/start_test');
+});
+
+app.get('/aggregate_first', function(request, response) {
+  response.render('pages/aggregate_first');
+});
+
+app.get('/aggregate_second', function(request, response) {
+  response.render('pages/aggregate_second');
+});
+
+
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
