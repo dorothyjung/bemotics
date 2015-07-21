c502d57c6f19705deae667a1e5067ca0445ae029
Dorothy Jung <jungy@berkeley.edu>
2015-07-21 17:29:45 +0200

    changed test data

--- index.js
+++ index.js
@@ -64,6 +64,18 @@ app.get('/db', function (request, response) {
   });
 });
 
+app.post('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_data_1', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
